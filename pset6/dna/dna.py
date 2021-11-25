49100d59c3edfb85bf9d66224940442c577bdc1b
Aaron Daughtry <aarondaughtry@gmail.com>
2021-11-24 19:41:58 -0500

    Added docstring for DNA STR function

---
+++ pset6/dna/dna.py
@@ -0,0 +1,20 @@
+import csv
+
+
+def import_STR_data(filename):
+    """Imports a CSV file containing data on DNA Short Tandem Repeats (STRs).
+
+    Data should be formatted like so:
+    
+    name,[STR 1],[STR 2],...
+
+    with the cells underneath the STR headings containing integers for how many
+    times an STR repeats in a specific genome.
+
+    Args:
+        filename: A CSV file of STR DNA sequences
+
+    Returns:
+        A list of dicts with names and STR sequences.
+
+    """
